547e74ef8df74422d92dee903380682235788a94
kocht <azizkocht28@gmail.com>
2025-07-07 06:29:38 +0100

    Add files via upload

---
+++ event.js
@@ -0,0 +1,19 @@
+function msg() {
+    alert('Bienvenue dans notre page');
+}
+
+function grandir() {
+    document.getElementById('img1').style.width = "200px";
+}
+
+function reduire() {
+    document.getElementById('img1').style.width = "150px";
+}
+
+function changerImg() {
+    // Optional: add logic here to change image on mouseover if desired
+}
+
+function imgInit() {
+    // Optional: reset image or revert changes on mouseout if needed
+}
